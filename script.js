cbaab3ec5461e7386f85b51f2ebcf6f472b0f7c7
agrawaldisha <agrawaldisha294@gmail.com>
2022-11-25 12:36:17 +0530

    commit

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+import {getDatabase,ref,get,set,update,remove} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+   
+ const db=getDatabase();
+  
+ var txtV,emailV,numV,subV,msgV
+
+ function sendMsg(event)
+{
+    event.preventDefault();
+    readFormData();
+    //code to send data to database
+
+    set(ref(db,"data/" + numV),{
+        txt:txtV,email:emailV,num:numV,
+        sub:subV,
+        msg:msgV
+    }).then(()=>{
+        alert("data stored")
+    }).catch((error)=>{
+        alert("Unsuccessful",error);
+    });
+
+    clearFormData();
+}
+
+
+
 let searchBtn = document.querySelector('#search-btn');
 let searchBar = document.querySelector('.search-bar-container');
 let formBtn = document.querySelector('#login-btn');
@@ -34,3 +61,41 @@ formClose.addEventListener('click', () =>{
     loginForm.classList.remove('active');
 });
 
+
+//fireBase code for contact
+
+//getting data from i/p
+
+var txtBox=document.getElementById('txt');
+var emailBox=document.getElementById('email');
+var numBox=document.getElementById('num');
+var subBox=document.getElementById('sub');
+var msgBox=document.getElementById('msg');
+
+//getting refrences from button
+// var btns=document.getElementById('btn').onclick=sendMsg;
+document.getElementById("fff").addEventListener("submit",sendMsg);
+
+
+
+//read data from form
+function readFormData()
+{
+    txtV=txtBox.value;
+    emailV=emailBox.value;
+    numV=numBox.value;
+    subV=subBox.value;
+    msgV=msgBox.value;
+    console.log(txtV,emailV,numV,subV,msgV);
+}
+
+//clear data from form
+
+function clearFormData()
+{
+    txtBox.value="";
+    emailBox.value="";
+    numBox.value="";
+    subBox.value="";
+    msgBox.value="";
+}
